97b47c890066e922a3d4513cd1bc586a90ef29e2
rfpotrero <raulf.potrero@gmail.com>
2022-11-28 10:44:38 +0000

    feat: Add models and template to products app
    Adding models and memberships template to products app. This includes
    category and products. #17

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def memberships(request):
+    return render(request, 'products/memberships.html')
--- stronglifts/urls.py
+++ stronglifts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('website.urls')),
+    path('products/', include('products.urls')),
 ]
