53fa97f12ec8ba0ceb00556bc1157276c8c1483b
rfpotrero <raulf.potrero@gmail.com>
2022-11-28 16:39:39 +0000

    feat: Add model for products and categories
    Added models for products and categorires prior implementing the
    fixtures to load data to the dabatase. #17

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    
+  
     class Meta:
         verbose_name_plural: 'Categories'
 
@@ -11,7 +11,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
+
     category = models.ForeignKey('Category',
                                  null=True, blank=True,
                                  on_delete=models.SET_NULL)
