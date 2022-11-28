9cb0f573b8d359e294f97e6b3cb4abeb26adf141
rfpotrero <raulf.potrero@gmail.com>
2022-11-28 16:31:08 +0000

    feat: Attach postgresql database to project
    Attached elephantsql database to project added link to env.py. #20

--- stronglifts/settings.py
+++ stronglifts/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.isfile("env.py"):
     import env
@@ -86,12 +87,8 @@ WSGI_APPLICATION = 'stronglifts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
