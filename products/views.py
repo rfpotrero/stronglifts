335aa1e81c36e40741d1a4bb880c0726250cb1fc
rfpotrero <raulf.potrero@gmail.com>
2022-11-29 15:29:48 +0000

    Create page to display all memberships
    Page to display all  membership created. Including adding custom css
    to handle padding and modified bootstrap card. #18

--- products/views.py
+++ products/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def memberships(request):
-    return render(request, 'products/memberships.html')
+    """ View To display all memberships """
+
+    all_memberships = Product.objects.filter(category="1")
+
+    context = {
+        'all_memberships': all_memberships
+    }
+    return render(request, 'products/memberships.html', context)
