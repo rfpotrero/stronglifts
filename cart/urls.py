0cec2fd0cc53a9037a0142a4f50228575aec2135
rfpotrero <raulf.potrero@gmail.com>
2022-12-13 16:52:25 +0000

    feat: Create code for add to cart items
    Created the code for add to cart items that will be used
    along the view_cart view #29

--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('view_cart', views.view_cart, name='view_cart'),
+    path('add/<product_id>', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def view_cart(request):
+    """ View to display the cart """
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, product_id):
+    """ Add to cart products view """
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if product_id in list(cart.keys()):
+        cart[product_id] += 1
+    else:
+        cart[product_id] = 1
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
