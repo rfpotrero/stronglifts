f639d5edbfb05580c172052bc6b8b2e5861646e4
rfpotrero <raulf.potrero@gmail.com>
2022-12-14 11:29:59 +0000

    fix: Fix display per item and cart total
    Add table in cart and fixed display total per item and cart total
    in template. #26

--- cart/contexts.py
+++ cart/contexts.py
@@ -2,28 +2,29 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-def cart_contents(request):
 
+def cart_contents(request):
+    """ Context to display items in the cart view """
     cart_items = []
     total = 0
-    product_count = 0
     cart = request.session.get('cart', {})
+    cart_total = 0
 
     for product_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=product_id)
-        total += quantity * product.price
-        product_count += quantity
+        total = quantity * product.price
         cart_items.append({
             'product_id': product_id,
             'quantity': quantity,
             'product': product,
+            'total': total,
         })
+        cart_total += total
 
     context = {
         'cart_items': cart_items,
-        'total': total,
-        'product_count': product_count,
+        'cart_total': cart_total
     }
 
+    print(cart_total)
     return context
-    
