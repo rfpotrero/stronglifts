9ae4e24bdbecf2540f614f645eeae57090ac892c
rfpotrero <raulf.potrero@gmail.com>
2022-12-28 15:27:23 +0000

    feat: Create forms.py and include order form
    Created Order form with custom init in order to use custom
    placeholders. #34

---
+++ checkout/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    model = Order
+    fields = {'full_name', 'email', 'phone_number',
+              'street_address1', 'street_address2',
+              'city', 'postcode', 'county', 'country'}
+
+    
+def __init__(self, *args, **kwargs):
+    """
+    Override default init to placeholders and labels
+    """
+    super().__init__(*args, **kwargs)
+    placeholders = {
+        'full_name': 'Full Name',
+        'email': 'Email Address',
+        'phone_number': 'Phone Number',
+        'street_address1': 'Street Address 1', 
+        'street_address2': 'Street Address 2',
+        'city': 'City',
+        'postcode': 'Postcode',
+        'county': 'County',
+        'country': 'Country'
+    }
+
+    self.fields['full_name'].widget.attrs['autofocus'] = True
+    for field in self.fields:
+        if self.fields[field].required:
+            placeholder = f'{placeholders[field]} *'
+        else:
+            placeholder = placeholders[field]
+
+        self.fields[field].widget.attrs['placeholder'] = placeholder
+        self.fields[field].label = False
