8eb5d293edd010f12c49573f1988ea1d84fac142
rfpotrero <raulf.potrero@gmail.com>
2022-12-28 16:50:55 +0000

    fix: Correct typo on the orderlineitem models
    Correcting a typo on the orderlineitem model. On_delete field had typo
    that prevented django to start the server.

--- checkout/models.py
+++ checkout/models.py
@@ -47,9 +47,9 @@ class Order(models.Model):
         super().save(*args, **kwargs)
 
 
-class OderLineItem(models.Model):
+class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=CASCADE)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
