d407a8186d1ba46b5178a98f48aa7c86575ed721
rfpotrero <raulf.potrero@gmail.com>
2022-12-30 17:36:40 +0000

    feat: Add stripe jsscript and apikeys to env file
    Modified base.html template to load stripe cnd js script and add
    the api keys to env variable to ensure keep them outside git. #37

--- stronglifts/settings.py
+++ stronglifts/settings.py
@@ -147,3 +147,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe Info
+
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_CURRENCY = 'eur'
