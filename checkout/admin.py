5e7c71c2a5bbdb918be050c857a60756d89f7719
rfpotrero <raulf.potrero@gmail.com>
2023-01-04 08:55:10 +0000

    style: Update checkout admin to display orders
    Update checkout admin to allow admins to display orders. #38

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Order, OrderLineItem
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date')
+
+    fields = ('order_number', 'date')
+
+    list_display = ('order_number', 'date', 'order_total', 'email', 'full_name')
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
